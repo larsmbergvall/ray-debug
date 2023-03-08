06efd25d4a15106d8fc6fb9731b4a16e13d1f9ea
Lars Bergvall <lars.bergvall@academedia.se>
2023-03-08 16:56:05 +0100

    added some simple tests for the ray request

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,8 +1,10 @@
 use serde::{Deserialize, Serialize};
 
+mod helpers;
 mod integration;
 mod meta_test;
 mod origin_test;
+mod ray_request_test;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TestUser {
