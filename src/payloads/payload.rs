70fd594d319d3c94f16561887abc073087cfd9c3
Lars Bergvall <lars.bergvall@academedia.se>
2023-03-09 10:15:21 +0100

    added color payloads and the ability to override a payloads origin object (mostly for testing purposes)

--- src/payloads/payload.rs
+++ src/payloads/payload.rs
@@ -1,3 +1,5 @@
+use crate::origin::Origin;
+use crate::payloads::color_payload::ColorPayload;
 use crate::payloads::html_payload::HtmlPayload;
 use crate::payloads::log_payload::LogPayload;
 use serde::Serialize;
@@ -7,4 +9,15 @@ use serde::Serialize;
 pub enum Payload {
     Log(LogPayload),
     Html(HtmlPayload),
+    Color(ColorPayload),
+}
+
+impl Payload {
+    pub fn set_origin(&mut self, origin: Origin) {
+        match self {
+            Payload::Log(v) => v.origin = origin,
+            Payload::Html(v) => v.origin = origin,
+            Payload::Color(v) => v.origin = origin,
+        }
+    }
 }
