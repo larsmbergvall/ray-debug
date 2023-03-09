8fdaec4b135260d95234ed027337cda3466a0f35
Lars Bergvall <lars.bergvall@academedia.se>
2023-03-09 10:14:42 +0100

    made the meta test less hardcoded

--- src/tests/meta_test.rs
+++ src/tests/meta_test.rs
@@ -3,15 +3,17 @@ use rustc_version::version_meta;
 
 #[test]
 fn it_gets_correct_data() {
-    let version = version_meta();
-    let expected = match version {
+    let rustc_version = match version_meta() {
         Ok(version_meta) => version_meta.semver.to_string(),
         _ => panic!("Could not get version"),
     };
 
     let meta = Meta::default();
 
-    assert_eq!(expected, meta.rustc_version);
-    assert_eq!("ray", meta.project_name);
-    assert_eq!("0.1.0", meta.project_version);
+    let pkg_name = option_env!("CARGO_PKG_NAME").unwrap_or("should fail if it gets this value");
+    let pkg_version = option_env!("CARGO_PKG_VERSION").unwrap_or("this too");
+
+    assert_eq!(rustc_version, meta.rustc_version);
+    assert_eq!(pkg_name, meta.project_name);
+    assert_eq!(pkg_version, meta.project_version);
 }
