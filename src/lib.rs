e8d1cecfb94eb44b77840a7f69848dd3e04b0b2b
Lars Bergvall <lars.bergvall@academedia.se>
2023-03-13 11:04:56 +0100

    made a more generic Rayable trait; the ray() function now takes most stuff as an argument and attempts to turn it into a ray request

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,22 @@ use std::error::Error;
 use crate::meta::Meta;
 use crate::payloads::log_payload::LogPayload;
 use crate::payloads::payload::Payload;
-use serde::Serialize;
 
 use crate::ray_request::RayRequest;
+use crate::rayable::Rayable;
 
 mod ray_color;
 mod ray_request;
 
+#[deprecated(since = "0.0.4", note = "This will be removed; use ray() instead")]
 pub fn ray_log<T: Into<String>>(value: T) -> Result<RayRequest, Box<dyn Error>> {
     RayRequest::log(value.into(), None).send()
 }
 
-pub fn ray<T: Serialize>(value: &T) -> Result<RayRequest, Box<dyn Error>> {
-    let json = helpers::get_json(value, false);
-    let serde_value = serde_json::from_str(&json).unwrap();
+pub fn ray<T: Rayable>(value: T) -> Result<RayRequest, Box<dyn Error>> {
+    let request: RayRequest = value.into_ray_request();
 
-    RayRequest::html(helpers::value_to_html(&serde_value), None).send()
+    request.send()
 }
 
 pub fn ray_charles() -> Result<RayRequest, Box<dyn Error>> {
@@ -35,5 +35,6 @@ mod helpers;
 mod meta;
 mod origin;
 mod payloads;
+pub mod rayable;
 #[cfg(test)]
 mod tests;
